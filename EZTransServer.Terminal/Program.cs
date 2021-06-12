40e840fdc392fb7e842f7f7818e5ed7706f53288
Syri <junimiso04@naver.com>
2021-06-12 13:58:09 +0900

    fix: modify project structure

---
+++ EZTransServer.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EZTransServer.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
